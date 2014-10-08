72fbbe8fbe9b326690d0eb946fa24c1b6d62fff1
madsoulswe <madsoulswe@hotmail.com>
2014-10-08 17:18:45 +0200

    Added shading, fog, rebuilding custom controller

--- trunk/Flight/Scripts/core.js
+++ trunk/Flight/Scripts/core.js
@@ -20,7 +20,16 @@ if (!window.requestAnimationFrame) {
 
 
 Flight.Settings = {
-    BackgroundColor: 0xffffff
+    BackgroundColor: 0xffffff,
+    SkyColor: 0x9cf2ff,
+    Ground: {
+        Color: 0xb39272,
+        Ambient: 0xe3d1bf,
+        Specular: 0x453322
+    },
+    OcenaColor: 0x7295b3,
+    Floor: 0,
+    MountainScale: 1000
 }
 
 Flight.Utilities = (function ($, window, document, undefined) {
@@ -42,25 +51,6 @@ Flight.Utilities = (function ($, window, document, undefined) {
             }
         },
         Generate: {
-            Topography: function (width, height) {
-                var data = new Uint8Array(width * height),
-                    perlin = new SimplexNoise(),
-                    size = width * height,
-                    quality = 2,
-                    z = Math.random() * 100;
-
-                for (var j = 0; j < 4; j++) {
-                    quality *= 4;
-
-                    for (var i = 0; i < size; i++) {
-                        var x = i % width, y = ~~(i / width);
-                        data[i] += Math.abs(perlin.noise(x / quality, y / quality, z) * 0.5) * quality + 10;
-                    }
-
-                }
-
-                return data;
-            },
 
             RandomRange: function (min, max) { 
 	            return Math.random()*(max-min) + min; 
@@ -131,21 +121,28 @@ Flight.Utilities = (function ($, window, document, undefined) {
                 var context = canvas.getContext("2d");
                 context.drawImage(img,0,0);
 
-                var imageData = context.getImageData(0, 0, width, height).data;
-                
-                var j = 0;
+                var imageData = context.getImageData(0, 0, width, height).data,
+                    color = 0,
+                    j = 0;
+
                 for (var i = 0; i < imageData.length; i +=4) {
-                    var all = imageData[i] + imageData[i + 1] + imageData[i + 2];
-                    data[j++] =  (all / 30) * scale;
+                    color = imageData[i] + imageData[i + 1] + imageData[i + 2];
+                    data[j++] = (color / 765) * scale;
                 }
 
                 return data;
             },
 
-            AppendHeightData: function (plane, heightData) {
+            AppendHeightData: function (plane, heightData, vertex) {
+
                 for (var i = 0, l = plane.vertices.length; i < l; i++) {
-                    plane.vertices[i].z = heightData[i];
+                    plane.vertices[i][vertex] = heightData[i];
                 }
+
+                plane.verticesNeedUpdate = true;
+                plane.computeFaceNormals();
+                plane.computeVertexNormals();
+                plane.normalsNeedUpdate = true;
 
                 return plane;
             }
@@ -159,34 +156,32 @@ Flight.Scene = (function ($, window, document, undefined) {
         var _this = this;
 
         this.scene = new THREE.Scene({ fixedTimeStep: 1 / 120 });
-       // this.scene.fog = new THREE.FogExp2(Flight.Settings.BackgroundColor, 0.005);
+        this.scene.fog = new THREE.FogExp2(Flight.Settings.BackgroundColor, 0.001);
 
-        this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
         this.camera.position.set(0, 25, 0);
+        
 
-
-        var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.9);
-        hemiLight.position.set(100, 1000, 100);
+        var hemiLight = new THREE.HemisphereLight(Flight.Settings.SkyColor, Flight.Settings.BackgroundColor, 0.5);
+        hemiLight.position.set(0, 2000, 0);
+        hemiLight.castShadow = true;
 
         this.scene.add(hemiLight);
+        /*
+
+        var dirLight = new THREE.DirectionalLight(Flight.Settings.BackgroundColor, 0.8);
+        dirLight.castShadow = true;
+
+        this.scene.add(dirLight);
+        */
+        //var light = new THREE.DirectionalLight(0xffffff);
+        //light.position.set(0, 500, 500).normalize();
+        //this.scene.add(light);
 
 
-        var spotLight = new THREE.SpotLight(0xffffff);
-        spotLight.position.set(100, 1000, 100);
-
-        spotLight.castShadow = true;
-
-        spotLight.shadowMapWidth = 1024;
-        spotLight.shadowMapHeight = 1024;
-
-        spotLight.shadowCameraNear = 500;
-        spotLight.shadowCameraFar = 4000;
-        spotLight.shadowCameraFov = 300;
-
-        this.scene.add(spotLight);
 
 
-        this.renderer = new THREE.WebGLRenderer({ antialias: false, autoClear: false });
+        this.renderer = new THREE.WebGLRenderer();
         this.renderer.setClearColor(Flight.Settings.BackgroundColor);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -215,24 +210,6 @@ Flight.Scene = (function ($, window, document, undefined) {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    Scene.prototype.AddMesh = function (geometry, scale, x, y, z, rx, ry, rz, material) {
-
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.scale.x = mesh.scale.y = mesh.scale.z = scale;
-        mesh.position.x = x;
-        mesh.position.y = y;
-        mesh.position.z = z;
-        mesh.rotation.x = rx;
-        mesh.rotation.y = ry;
-        mesh.rotation.z = rz;
-        mesh.overdraw = true;
-        mesh.doubleSided = false;
-        mesh.updateMatrix();
-
-        this.scene.addObject(mesh);
-
-        return mesh;
-    }
 
     return Scene;
 
@@ -243,15 +220,35 @@ Flight.Player = (function ($, window, document, undefined) {
 
     function Player(scene) {
         var _this = this;
+
+        _this.scene = scene;
+        _this.moveForward = false;
+        _this.moveLeft = false;
+        _this.moveBackward = false;
+        _this.moveRight = false;
+
+        _this.yawLeft = false;
+        _this.yawRight = false;
+        
+        _this.rollLeft = false;
+        _this.rollRight = false;
+
+        _this.pitchUp = false;
+        _this.pitchDown = false;
+
+        _this.rollRad = THREE.Math.degToRad(10);
+        _this.rollTween = new TWEEN.Tween(_this.scene.camera.rotation).easing(TWEEN.Easing.Cubic.Out);
+
+        _this.pitchRad = THREE.Math.degToRad(10);
+        _this.pitchTween = new TWEEN.Tween(_this.scene.camera.rotation).easing(TWEEN.Easing.Cubic.Out);
+        
+        _this.scene.camera.rotationAutoUpdate = true;
+        /*
         this.controls = new THREE.FirstPersonControls(scene.camera);
         this.controls.movementSpeed = 100;
         this.controls.lookSpeed = 0.1;
         this.controls.tiltFactor = 0.05;
-        this.controls.autoForward = true;
-        this.controls.bounding = new THREE.Box3(
-            new THREE.Vector3(-500, 10, -500),
-            new THREE.Vector3(500, 100, 500)
-        )
+        this.controls.autoForward = false;*/
 
         $(window).on("resize", function () {
             _this.Resize.call(_this);
@@ -264,15 +261,145 @@ Flight.Player = (function ($, window, document, undefined) {
         scene.scene.addEventListener('resize', function (event) {
             _this.Resize.call(_this);
         });
+
+        _this.Controllers();
     }
 
+    Player.prototype.Controllers = function () {
+        var _this = this;
+
+        window.addEventListener('keydown', bind(this, _this.onKeyDown), false);
+        window.addEventListener('keyup', bind(this, _this.onKeyUp), false);
+
+        function bind(scope, fn) {
+
+            return function () {
+
+                fn.apply(scope, arguments);
+
+            };
+
+        };
+    }
+
+
+    /*
+    * a        65 (yaw)
+    * d        68
+    * w        87 (yaw)
+    * s        83
+    * 
+    * left     37 (roll)
+    * right    39 (roll)
+    * up       38 (pitch down)
+    * down     40 (pitch up)
+    */
+
+    Player.prototype.onKeyDown = function (event) {
+        var _this = this;
+
+        switch (event.keyCode) {
+            case 65: _this.yawLeft = true; break;
+            case 68: _this.yawRight = true; break;
+
+            case 87: _this.moveForward = true; break;
+            case 83: _this.moveBackward = true; break;
+
+            case 38:
+                if (_this.pitchDown)
+                    break;
+
+                _this.pitchDown = true;
+                //_this.pitchTween.stop().to({ x: -_this.pitchRad }, 500).start();
+                
+                break;
+            case 40:
+                if (_this.pitchUp)
+                    break;
+
+                _this.pitchUp = true;
+                //_this.pitchTween.stop().to({ x: _this.pitchRad }, 500).start();
+                
+                break;
+
+            case 37:
+                if (_this.rollLeft)
+                    break;
+
+                _this.rollLeft = true;
+                //_this.rollTween.stop().to({ z: _this.rollRad }, 500).start();
+                break;
+
+            case 39:
+                if (_this.rollRight)
+                    break;
+
+                _this.rollRight = true;
+               // _this.rollTween.stop().to({ z: -_this.rollRad }, 500).start();
+                break;
+        }
+
+    };
+
+    Player.prototype.onKeyUp = function (event) {
+        var _this = this;
+
+        switch (event.keyCode) {
+            case 65: _this.yawLeft = false; break;
+            case 68: _this.yawRight = false; break;
+
+            case 87: _this.moveForward = false; break;
+            case 83: _this.moveBackward = false; break;
+
+            case 38:
+            case 40:
+                _this.pitchUp = false; 
+                _this.pitchDown = false;
+
+                //_this.pitchTween.stop().to({ x: 0 }, 500).start();
+                break;
+
+            case 37:
+            case 39:
+                _this.rollLeft = false;
+                _this.rollRight = false;
+
+                //_this.rollTween.stop().to({ z: 0 }, 500).start();
+                break;
+        }
+
+    };
+
     Player.prototype.Update = function (delta) {
-        this.controls.update(delta);
-        
+        var _this = this;
+
+        if (_this.yawLeft && !_this.yawRight)
+            _this.scene.camera.rotation.y += 0.01;
+        else if (_this.yawRight && !_this.yawLeft)
+            _this.scene.camera.rotation.y -= 0.01;
+
+        if (_this.rollLeft && !_this.rollRight)
+            _this.scene.camera.rotation.y += 0.01;
+        else if (_this.rollRight && !_this.rollLeft)
+            _this.scene.camera.rotation.y -= 0.01;
+
+        if (_this.pitchDown && !_this.pitchUp)
+            _this.scene.camera.rotation.x += 0.01;
+        else if (_this.pitchUp && !_this.pitchDown)
+            _this.scene.camera.rotation.x -= 0.01;
+
+        if (_this.moveForward)
+            _this.scene.camera.translateZ(-5);
+        else if (_this.moveBackward)
+            _this.scene.camera.translateZ(5);
+
+        if (this.controls != undefined)
+            this.controls.update(delta);
     }
 
     Player.prototype.Resize = function () {
-        this.controls.handleResize();
+        if(this.controls != undefined)
+            this.controls.handleResize();
     }
 
     return Player;
@@ -308,7 +435,6 @@ Flight.Element = (function ($, window, document, undefined) {
 
 })(jQuery, window, document);
 
-
 Flight.ElementIterator = (function ($, window, document, undefined) {
     
     function ElementIterator(scene, settings) {
@@ -334,44 +460,71 @@ Flight.World = (function ($, window, document, undefined) {
     function World(scene) {
         var _this = this;
 
-        this.materials = [];
-        this.currenMaterial = 0;
+        _this.scene = scene;
 
+        this.AddGround(function () {
+            this.AddOcean();
+        });
+    }
 
-        var imageLoader = new THREE.ImageLoader();
+    World.prototype.AddGround = function (complete) {
+        var _this = this;
+
         Flight.Utilities.Load.Image("/assets/textures/mountain_heightmap_300.jpg", function () {
 
-            var heightData = Flight.Utilities.Generate.HeightData(this, 300, 300, 100);
+            var heightData = Flight.Utilities.Generate.HeightData(this, 301, 301, Flight.Settings.MountainScale);
+            var groundPlane = new THREE.PlaneGeometry(10000, 10000, 300, 300);
 
-            _this.plane = new THREE.PlaneGeometry(10000, 10000, 300, 300);
+            Flight.Utilities.Generate.AppendHeightData(groundPlane, heightData, "z");
 
-            Flight.Utilities.Generate.AppendHeightData(_this.plane, heightData);
-
-            var texture = THREE.ImageUtils.loadTexture("/assets/textures/mountain_300.jpg", THREE.UVMapping, function (text) {
+            THREE.ImageUtils.loadTexture("/assets/textures/ground.gif", THREE.UVMapping, function (text) {
                 text.wrapS = THREE.RepeatWrapping;
                 text.wrapT = THREE.RepeatWrapping;
+                text.needsUpdate = true;
 
-                var mountainMaterial = new THREE.MeshPhongMaterial({
-                    map: texture,
-                    ambient: 0xaaaaaa,
-                    specular: 0xffffff,
-                    shininess: 0,
-                    shading: THREE.SmoothShading
+                var material = new THREE.MeshPhongMaterial({
+                    color: Flight.Settings.Ground.Color,
+                    ambient: Flight.Settings.Ground.Ambient,
+                    specular: Flight.Settings.Ground.Specular,
+                    shading: THREE.FlatShading
                 });
+                
+                _this.ground = new THREE.Mesh(
+                    groundPlane, 
+                    material
+                )
 
-                this.ground = new THREE.Mesh(_this.plane, mountainMaterial);
+                _this.ground.rotation.x = -Math.PI / 2;
+                _this.ground.position.y = Flight.Settings.Floor - (Flight.Settings.MountainScale / 2);
+                _this.ground.castShadow = false;
+                _this.ground.receiveShadow = true;
 
-                this.ground.position.y = 0;
-                this.ground.rotation.x = -Math.PI / 2;
-
-                scene.scene.add(this.ground);
+                _this.scene.scene.add(_this.ground);
             });
 
-            
-            
-        });
+            if (typeof (complete) == "function")
+                complete.call(_this);
 
-        
+        });
+    }
+
+    World.prototype.AddOcean = function (complete) {
+        var _this = this;
+
+        _this.ocean = new THREE.Mesh(
+            new THREE.PlaneGeometry(10000, 10000, 10, 10),
+            new THREE.MeshPhongMaterial({ color: Flight.Settings.OcenaColor, ambient: 0x666666, specular: 0xffffff,  opacity: 1, shininess: 1, shading: THREE.FlatShading })
+        );
+
+        _this.ocean.rotation.x = -Math.PI / 2;
+        _this.ocean.position.y = Flight.Settings.Floor;
+
+        _this.scene.scene.add(_this.ocean);
+
+        new TWEEN.Tween(_this.ocean.position).easing(TWEEN.Easing.Cubic.InOut).yoyo(true).repeat(10000).to({ y: Flight.Settings.Floor - 10 }, 10000).start();
+
+        if (typeof (complete) == "function")
+            complete();
     }
 
     return World;
@@ -403,6 +556,8 @@ Flight.Ticker = (function ($, window, document, undefined) {
     Ticker.prototype.Tick = function () {
         var delta = this.clock.getDelta(),
             time = this.clock.getElapsedTime() * 5;
+
+        TWEEN.update();
 
         this.tick.call(this, delta, time);
     }
@@ -438,13 +593,13 @@ Flight.Core = (function ($, window, document, undefined) {
             complete: function () {
                 scene = new Flight.Scene(container[0]);
                 world = new Flight.World(scene);
-                elements = new Flight.ElementIterator(scene, {
+                /*elements = new Flight.ElementIterator(scene, {
                     count: 100,
                     bounding: new THREE.Box3(
                         new THREE.Vector3(-500, 10, -500),
                         new THREE.Vector3(500, 100, 500)
                     )
-                });
+                });*/
 
                 player = new Flight.Player(scene);
             },
